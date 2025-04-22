ce943fa0597349e9a6204590e8a7978e1d591033
YursKsf1 <yurs.ksf@gmail.com>
2025-04-21 20:01:29 -0500

    definir rutas (url) y templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', TemplateView.as_view(template_name='myapp/index.html')),
+    path('itinerario', TemplateView.as_view(template_name='myapp/itinerario.html')),
+    path('videos', TemplateView.as_view(template_name='myapp/videos.html')),
 ]
