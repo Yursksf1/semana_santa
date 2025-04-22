ba2fa0cd228cd7804e2d3220e49eed1a51c7bd6a
YursKsf1 <yurs.ksf@gmail.com>
2025-04-21 19:46:55 -0500

    definir modelo de video y agregarlo al admin

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    name = models.CharField(
+        max_length=500,
+        verbose_name="Nombre Video"
+    )
+    description = models.TextField(
+        verbose_name="Descripcion Video"
+    )
+
+    def __str__(self):
+        return self.name
